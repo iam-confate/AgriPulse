2d4fcc886194a78cebd0c4b7f0ad67a2e7c7acd4
confate <m>
2025-10-15 15:12:02 +0200

    Changed ContentView() to launchScreen().

--- AgriPulse App/Launch Screen.swift
+++ AgriPulse App/Launch Screen.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LaunchScreen: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    LaunchScreen()
 }
