6a1a92ecafb3978c2e1dd817fb668b406cfa5c2e
confate <m>
2025-10-15 15:07:25 +0200

    Initial Commit

---
+++ AgriPulse App/AgriPulse_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgriPulse_AppApp.swift
+//  AgriPulse App
+//
+//  Created by Confate Muleya  on 15/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct AgriPulse_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
