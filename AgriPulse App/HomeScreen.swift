a7c20daf51e46e962d962b4cfd7c0412123694e9
confate <m>
2025-10-15 15:17:54 +0200

    Created HomeScreen page and Price Comparison Page

---
+++ AgriPulse App/HomeScreen.swift
@@ -0,0 +1,18 @@
+//
+//  HomeScreen.swift
+//  AgriPulse App
+//
+//  Created by Confate Muleya  on 15/10/2025.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeScreen()
+}
---
+++ AgriPulse App/PriceComparisonPage.swift
@@ -0,0 +1,18 @@
+//
+//  PriceComparisonPage.swift
+//  AgriPulse App
+//
+//  Created by Confate Muleya  on 15/10/2025.
+//
+
+import SwiftUI
+
+struct PriceComparisonPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PriceComparisonPage()
+}
